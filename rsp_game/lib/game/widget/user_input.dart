af4977788db4eac47cb24712f510e06777e3965e
sik9252 <sik9252@naver.com>
2024-01-03 01:12:52 +0900

    feat: user_input > 유저의 가위바위보 입력 ui 구현

---
+++ rsp_game/lib/game/widget/user_input.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:rsp_game/game/enum.dart';
+
+import 'input_card.dart';
+
+class UserInput extends StatelessWidget {
+  final bool isDone;
+  final Function(InputType) callback;
+
+  const UserInput({required this.isDone, required this.callback, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (isDone) {
+      return Placeholder();
+    }
+
+    return Row(
+      children: _getInputs(callback),
+    );
+  }
+
+  List<Widget> _getInputs(Function(InputType) callback) {
+    return InputType.values
+        .map(
+          (type) => InputCard(
+            child: Image.asset(type.path),
+            callback: () => callback.call(type),
+          ),
+        )
+        .toList();
+  }
+}
