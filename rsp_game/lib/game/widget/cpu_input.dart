cb84a56ac994f3d347ddb2b0bda546bb49dfae08
sik9252 <sik9252@naver.com>
2024-01-03 12:33:07 +0900

    feat: cpu_input > 컴퓨터의 가위바위보 입력 ui 및 기능 구현

--- rsp_game/lib/game/widget/cpu_input.dart
+++ rsp_game/lib/game/widget/cpu_input.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../enum.dart';
+import 'input_card.dart';
+
 class CpuInput extends StatelessWidget {
   final bool isDone;
+  final InputType cpuInput;
 
-  const CpuInput({required this.isDone, super.key});
+  const CpuInput({required this.isDone, required this.cpuInput, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        // 빈 공간 만들때 shrink 좀 더 효과적
+        const Expanded(child: SizedBox.shrink()),
+        Expanded(
+          child: InputCard(child: getCpuInput()),
+        ),
+        const Expanded(child: SizedBox.shrink())
+      ],
+    );
+  }
+
+  Widget getCpuInput() {
+    if (isDone) {
+      return Image.asset((cpuInput.path));
+    }
+
+    return const SizedBox(
+      height: 80,
+      child: Center(
+        child: Text(
+          '?',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
