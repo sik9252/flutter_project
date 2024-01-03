ed242288bb0ddd465ff19a3c9404e0520a3d1c71
sik9252 <sik9252@naver.com>
2024-01-03 12:34:12 +0900

    feat: game_result > 게임 결과 표시 ui 및 기능 구현

--- rsp_game/lib/game/widget/game_result.dart
+++ rsp_game/lib/game/widget/game_result.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../enum.dart';
+
 class GameResult extends StatelessWidget {
   final bool isDone;
+  final Result? result;
+  final VoidCallback callback;
 
-  const GameResult({required this.isDone, super.key});
+  const GameResult(
+      {required this.isDone, this.result, required this.callback, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (isDone) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            result!.displayString,
+            style: const TextStyle(fontSize: 32),
+          ),
+          const SizedBox(height: 8),
+          TextButton(
+              onPressed: () => callback.call(),
+              child: const Text(
+                '다시 하기',
+                style: TextStyle(fontSize: 24, color: Colors.blue),
+              ))
+        ],
+      );
+    }
+
+    return const Center(
+      child: Text(
+        '가위, 바위 보 중 하나를 선택해 주세요.',
+        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
