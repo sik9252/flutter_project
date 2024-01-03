a4111ca3fa0aaa83b744da40cbf54dc405fb5013
sik9252 <sik9252@naver.com>
2024-01-03 12:33:44 +0900

    feat: user_input > 유저의 가위바위보 입력 기능 구현

--- rsp_game/lib/game/widget/user_input.dart
+++ rsp_game/lib/game/widget/user_input.dart
@@ -6,13 +6,22 @@ import 'input_card.dart';
 class UserInput extends StatelessWidget {
   final bool isDone;
   final Function(InputType) callback;
+  final InputType? userInput;
 
-  const UserInput({required this.isDone, required this.callback, super.key});
+  const UserInput(
+      {required this.isDone,
+      required this.callback,
+      this.userInput,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
     if (isDone) {
-      return Placeholder();
+      return Row(children: [
+        const Expanded(child: SizedBox.shrink()),
+        Expanded(child: InputCard(child: Image.asset(userInput!.path))),
+        const Expanded(child: SizedBox.shrink()),
+      ]);
     }
 
     return Row(
@@ -23,9 +32,13 @@ class UserInput extends StatelessWidget {
   List<Widget> _getInputs(Function(InputType) callback) {
     return InputType.values
         .map(
-          (type) => InputCard(
-            child: Image.asset(type.path),
-            callback: () => callback.call(type),
+          (type) => Expanded(
+            child: InkWell(
+              onTap: () => callback.call(type),
+              child: InputCard(
+                child: Image.asset(type.path),
+              ),
+            ),
           ),
         )
         .toList();
