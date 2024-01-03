72d4df0e50a9c67df8339e3e6ac0e746944c5acb
sik9252 <sik9252@naver.com>
2024-01-03 13:52:05 +0900

    인스타그램 ui 클론 프로젝트 구조 생성

---
+++ instagram_ui_clone/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstaCloneApp());
+}
+
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
